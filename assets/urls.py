f97e9fb9001f7d884051bd1347e64c958209acd2
ben104410 <titusben64@gmail.com>
2026-03-25 11:48:16 +0300

    verifications

--- assets/urls.py
+++ assets/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
-def _index_view(request, *args, **kwargs):
-    from .views import index
-    return index(request, *args, **kwargs)
+from .views import ListAssetsView, CreateAssetView, UpdateAssetView
 
 urlpatterns = [
-    path("", _index_view, name="assets-index"),
+    path("", ListAssetsView.as_view(), name="assets-index"),
+    path("all/", ListAssetsView.as_view(), name="list-assets"),
+    path("create/", CreateAssetView.as_view(), name="create-asset"),
+    path("update/<int:pk>/", UpdateAssetView.as_view(), name="update-asset"),
 ]
